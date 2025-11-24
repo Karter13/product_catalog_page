59f8f432e47e787b923bff1849062d7135d0539f
Karter13 <KarMiH1975@mail.ru>
2025-11-24 22:29:56 +0300

    DEV-010: Добавил обертку для разрешений свыше 2040px сетки rid

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import { ProductCard } from './ProductCard';
 import { Product } from '@/data/types/product';
@@ -25,28 +25,30 @@ export const ProductGrid = ({ products: allProducts }: ProductGridProps) => {
   }
 
   return (
-    <div className="
-    grid
-    grid-cols-1
-    sm:grid-cols-2
-    md:grid-cols-3
-    lg:grid-cols-4
-    xl:grid-cols-5
-    2xl:grid-cols-6
-    gap-6 p-6
-    ">
-      {currentProducts.map((product, index) => {
-        const isLast = index === currentProducts.length - 1;
-        return (
-          <div
-            key={product.id}
-            ref={isLast ? lastElementRef : null}
-            className="flex justify-center"
-          >
-            <ProductCard product={product} />
-          </div>
-        );
-      })}
+    <div className="mx-auto max-w-[2040px] w-full">
+      <div className="
+        grid
+        grid-cols-1
+        sm:grid-cols-2
+        md:grid-cols-3
+        lg:grid-cols-4
+        xl:grid-cols-5
+        2xl:grid-cols-6
+        gap-6 p-6"
+      >
+        {currentProducts.map((product, index) => {
+          const isLast = index === currentProducts.length - 1;
+          return (
+            <div
+              key={product.id}
+              ref={isLast ? lastElementRef : null}
+              className="flex justify-center"
+            >
+              <ProductCard product={product} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
